99777d9f942022d1ce1038ec9a3d72dd2e042e57
ElzoheeryAhmed <85372531+ElzoheeryAhmed@users.noreply.github.com>
2021-12-20 00:15:56 +0200

    Delete xmlConsistency.h

--- Individual codes/Checking the XML consistency/xmlConsistency.h
+++
@@ -1,8 +0,0 @@
-#include"xmlErrordetection.h"
-#include"xmlErrorcorrection.h"
-
-#pragma once
-
-bool xmlcorrect(LinkedList& l);
-void xml_errorPositions(LinkedList& lP, queue<string>& in_tagP, queue<int>& in_indexP, queue<int>& del_indexP);
-void xml_automatically_correct(LinkedList& l);
